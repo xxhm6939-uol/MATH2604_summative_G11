2d1b7d6389572d679f03588a318ac2a51e058a71
dsmf8385-uol <dsmf8385@leeds.ac.uk>
2026-03-19 11:46:56 +0000

    Created a Java file for Tridiagonals

---
+++ Tridiagonals.java
@@ -0,0 +1,16 @@
+import java.lang.Math*;
+
+class Tridiagonals
+{
+    static double [][] exampleMatrix(int n)
+    { 
+        double m[][] = new double [1][1];
+
+        
+
+
+        return m;
+    }
+
+
+}
