27141a8d0f5b2dc055bf842bbcf7715822ff2f42
dsmf8385-uol <dsmf8385@leeds.ac.uk>
2026-03-20 13:47:21 +0000

    Coded exampleMatrix() function

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -1,16 +1,28 @@
-import java.lang.Math*;
+import java.lang.Math;
 
 class Tridiagonals
 {
     static double [][] exampleMatrix(int n)
     { 
-        double m[][] = new double [1][1];
+        double m[][] = new double [3][n];
+        for (int j = 0 ; j < n ; j++)
+        { 
+            if (j < n-1)
+            { 
+                m[0][j] = 1;
+            }
 
-        
+            m[1][j] = -(j+1)*(j+1);
 
+            if (j > 0)
+            {
+                m[2][j] = j+2;
+            }
+        }
 
         return m;
+
     }
 
-
 }
+
