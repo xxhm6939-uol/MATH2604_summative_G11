0264b89a63f6424d40ecbdea48c08be0289d661e
Taran Kaur <dsmf8385@leeds.ac.uk>
2026-03-29 21:39:37 +0100

    Retrieved template file for Diagonals

--- Diagonals.java
+++ Diagonals.java
@@ -2,5 +2,10 @@ import static java.lang.Math*;
 
 class Diagonals 
 {
-    
+    public static double[] sum(double a[], double b[]) {
+        
+        double m[] = new double [a.length]
+        for (int i = 0; i)
+
+    }
 }
