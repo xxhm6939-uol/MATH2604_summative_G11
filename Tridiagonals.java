0dde1438e3cd8f71f0eb75a15e902789cad4d909
Taran Kaur <dsmf8385@leeds.ac.uk>
2026-04-04 18:22:43 +0100

    Added productWithDiagonal() function to Tridiagonals

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -2,26 +2,69 @@ import java.lang.Math;
 
 class Tridiagonals
 {
-    // Tridiagonals part (a)
+    // Part(a) - Example of a Tridiagonal Matrix
     static double [][] exampleMatrix(int n) 
     { 
-        double m[][] = new double [3][n];
-        for (int j = 0 ; j < n ; j++)
+        double m[][] = new double [3][n]; 
+        for (int i = 0; i < n; i++) // running through each position
         { 
-            if (j < n-1)
+            if (i < n - 1) 
             { 
-                m[0][j] = 1;
+                m[0][i] = 1; // all entries in superdiagonal = 1
             }
 
-            m[1][j] = -(j+1)*(j+1);
+            m[1][i] = - (i + 1) * (i + 1); // entries in main diagonal
 
-            if (j > 0)
+            if (i > 0)
             {
-                m[2][j] = j+2;
+                m[2][i] = i + 2; // all entried in subdiagonal
             }
         }
+        m[0][n - 1] = 0; // last element in superdiagonal
+        m[2][n - 1] = 0; // last element in subdiagonal
+
         return m;
     }
 
+    // Part(d) - Product of a Diagonal Matrix with Tridiagonal Matrix
+     static double m[][] productWithDiagonal(double d[], double t[][])
+    {
+        if (d == null || t == null) // returns null if any entries are null
+        {
+            return null;
+        }
+
+        int n = d.length;
+
+        if (t.length != 3) // null if number of diagonals doesn't match 3
+        {
+            return null;
+        }
+
+        if (t[0].length != n || t[1].length != n || t[2].length != n) // returns null if diagonals don't match in size
+        {
+            return null;
+        }
+
+        double m[][] = new double[3][n];
+        for (int i = 0; i < n; i++)
+        {
+            if (i < n - 1)
+            {
+                m[0][i] = d[i] * t[0][i]; // entries in superdiagonal
+            }
+
+            m[1][i] = d[i] * t[1][i]; // main diagonal
+
+            if (i > 0)
+            {
+                m[2][i - 1] = d[i] * t[2][i - 1]; // entried in subdiagonal
+            }
+        } 
+        m[0][n - 1] = 0; // entries in superdiagonal
+        m[2][n - 1] = 0; // entries in subdiagonal
+
+        return m;
+    }
 }
 
