9d3be53d892b635b67cfc209f763fde085af243b
xxhm6939-uol <xxhm6939@leeds.ac.uk>
2026-04-20 01:43:15 +0000

    Mi1 fixed Test_function

--- Test_function.java
+++ Test_function.java
@@ -1,13 +1,15 @@
-class Test { 
-    public static void main (String[] args) {
+class Test_function
+{ 
+    public static void main (String[] args)
+    {
         double m[][] = Tridiagonals.exampleMatrix(5);
         
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++)
+            {
             for (int j = 0; j < 5; j++){
                 System.out.print(m[i][j] + "");
             }
             System.out.println();
         }
-
     }
 }
